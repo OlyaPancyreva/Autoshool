8f2de366e768d06798a4f9016f53b9d39d5d770b
O'Lik <55744134+OlyaPancyreva@users.noreply.github.com>
2021-12-07 20:46:04 +0300

    project django + vue

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card mb-3 img" id="footer">
+    <div class="card-body" style="color: white;">
+      <p class="card-text">2021 @Автошкола "Drive"
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+#footer {
+  width: 1300px;
+  margin-top: 20px;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 10px;
+  background-color: #0f6890;
+}
+</style>
